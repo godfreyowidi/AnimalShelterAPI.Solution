db3e1933d55053019bedeebe9e886fbc2761a17c
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-08-21 02:52:23 +0300

    add method for user registration

---
+++ OrphanagePark/Controllers/AuthManagementController.cs
@@ -0,0 +1,77 @@
+using System.Text;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using OrphanagePark.Configuration;
+using OrphanagePark.Models.Dtos.Requests;
+using OrphanagePark.Models.Dtos.Responses;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System;
+
+namespace OrphanagePark.Controllers
+{
+  //api/authManagement
+  [Route("api/[controller]")]
+  public class AuthManagementController : ControllerBase
+  {
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly JwtConfig _jwtConfig;
+
+    public AuthManagementController(
+        UserManager<IdentityUser> userManager, 
+        IOptionsMonitor<JwtConfig> optionsMonitor)
+    {
+      _userManager = userManager;
+      _jwtConfig = optionsMonitor.CurrentValue;
+    }
+
+    public async Task<IActionResult> Register([FromBody] UserRegistrationDto user)
+    {
+      if (ModelState.IsValid)
+      {
+          //utilising model state
+          var existingUser = await _userManager.FindByEmailAsync(user.Email);
+
+          if (existingUser != null)
+          {
+              return BadRequest(new RegistrationResponse(){
+                Errors = new List<string>() {
+                  "Email already in use"
+                },
+                Success = false
+            });
+          }
+
+          var newUser = new IdentityUser() { Email = user.Email, UserName = user.Username};
+          var isCreated = await _userManager.CreateAsync(newUser, user.Password);
+          if (isCreated.Succeeded)
+          {
+            var jwtToken = GenerateJwtToken(newUser);
+
+            return Ok(new RegistrationResponse() {
+              Success = true,
+              Token = jwtToken
+            });
+          }else {
+            return BadRequest(new RegistrationResponse(){
+                Errors = isCreated.Errors.Select(x => x.Description).ToList(),
+                Success = false
+            });
+          }
+      }
+
+      return BadRequest(new RegistrationResponse(){
+              Errors = new List<string>() {
+                "Invalid payload"
+              },
+              Success = false
+      });
+    }
+
+  }
+}
