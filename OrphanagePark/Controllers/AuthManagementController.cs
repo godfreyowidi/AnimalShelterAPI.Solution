5da526c97021e628afc9033d16c3b4e81eb366f6
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-08-21 02:54:11 +0300

    add method to generate token to the controller

--- OrphanagePark/Controllers/AuthManagementController.cs
+++ OrphanagePark/Controllers/AuthManagementController.cs
@@ -30,6 +30,7 @@ namespace OrphanagePark.Controllers
       _jwtConfig = optionsMonitor.CurrentValue;
     }
 
+    //user registation
     public async Task<IActionResult> Register([FromBody] UserRegistrationDto user)
     {
       if (ModelState.IsValid)
@@ -73,5 +74,30 @@ namespace OrphanagePark.Controllers
       });
     }
 
+    //Generate token
+    private string GenerateJwtToken(IdentityUser user)
+    {
+      var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+      var key = Encoding.ASCII.GetBytes(_jwtConfig.Secret);
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(new []
+        {
+          new Claim("Id", user.Id),
+          new Claim(JwtRegisteredClaimNames.Email, user.Email),
+          new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+          new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        }),
+        Expires = DateTime.UtcNow.AddHours(6),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+      };
+
+      var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+      var jwtToken = jwtTokenHandler.WriteToken(token);
+
+      return jwtToken;
+    }
   }
 }
