94c6a78bed17446569652169fd9ec85bf2768bde
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-08-21 02:39:09 +0300

    add migrations for authentication properties

---
+++ OrphanagePark/Migrations/20210820233603_AddDtos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrphanagePark.Migrations
+{
+    public partial class AddDtos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
