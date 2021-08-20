e276c989b6b292bf68f47b87a06fe83c767fbd5c
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-08-21 00:37:59 +0300

    initial project setup

---
+++ OrphanagePark/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace OrphanagePark.Models
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string Type { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
